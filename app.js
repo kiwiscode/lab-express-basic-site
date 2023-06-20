36222e2965f9ed3e47d901592043dd2eeb2aaca1
Aykut Kav <ayktkav@gmail.com>
2023-06-20 23:08:41 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+// We create our own server named app
+// Express server will be handling requests and responses
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/home", (request, response, next) => {
+  response.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/about", (request, response, next) => {
+  response.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (request, response, next) => {
+  response.sendFile(__dirname + "/views/work.html");
+});
+
+// ... the previously added code
+// Server Started
+app.listen(3000, () => console.log("My first app listening on port 3000! "));
